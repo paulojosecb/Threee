da05b6893ceff3f8da945d01fbb47b27d0d017c5
Paulo José <paulojocb@gmail.com>
2019-06-27 19:19:08 -0300

    Create Item.swift

---
+++ Threee/Model/Item.swift
@@ -0,0 +1,21 @@
+//
+//  Item.swift
+//  Threee
+//
+//  Created by Paulo José on 27/06/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var checked: Bool
+    var name: String
+    
+    init() {
+        name = ""
+        checked = false
+    }
+    
+}
