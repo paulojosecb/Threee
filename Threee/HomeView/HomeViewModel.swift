1ba0a45ecd3b6ae1dfca1063500f57204f14014e
Paulo José <paulojocb@gmail.com>
2019-06-27 19:03:33 -0300

    Create HomeViewModel.swift

---
+++ Threee/HomeView/HomeViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewModel.swift
+//  Threee
+//
+//  Created by Paulo José on 27/06/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel: NSObject {
+    
+    
+    
+}
