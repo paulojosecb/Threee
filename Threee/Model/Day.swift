ed0d29d23c3010b064d2c081df99783af607bc17
Paulo José <paulojocb@gmail.com>
2019-06-27 19:19:56 -0300

    Update Day.swift

--- Threee/Model/Day.swift
+++ Threee/Model/Day.swift
@@ -11,17 +11,32 @@ import Foundation
 class Day {
     
     let date: Double
-    var item1: String = ""
-    var item2: String = ""
-    var item3: String = ""
+    var items = [Item]()
     
     init(daysFromNow: Int) {
+        
         guard let today = Calendar.current.date(byAdding: .day, value: daysFromNow, to: Date()) else {
             self.date = Date().timeIntervalSince1970
             return
             
         }
         self.date = today.timeIntervalSince1970
+        self.items = [Item(), Item(), Item()]
+    }
+    
+    func isDayCompleted() -> Bool {
+        
+        return items.reduce(true, { (accumulator, item) -> Bool in
+            return accumulator && item.checked
+        })
+    }
+    
+    func edit(item: Int, newValue: String) {
+        items[item].name = newValue
+    }
+    
+    func toggle(item: Int) {
+        items[item].checked = !items[item].checked
     }
     
 }
