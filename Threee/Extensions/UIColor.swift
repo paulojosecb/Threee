5a0c7d31485e546f6e028e646361e6204bdb8e7c
Paulo José <paulojocb@gmail.com>
2019-06-30 17:54:01 -0300

    Create UIColor.swift

---
+++ Threee/Extensions/UIColor.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var yellow: UIColor {
+        return UIColor(red: 1.0, green: 221.0 / 255.0, blue: 86.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var black: UIColor {
+        return UIColor(white: 0.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var grey: UIColor {
+        return UIColor(white: 128.0 / 255.0, alpha: 0.2)
+    }
+    
+    @nonobjc class var white: UIColor {
+        return UIColor(red: 245.0 / 255.0, green: 242.0 / 255.0, blue: 235.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var salmon: UIColor {
+        return UIColor(red: 243.0 / 255.0, green: 170.0 / 255.0, blue: 159.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var green: UIColor {
+        return UIColor(red: 151.0 / 255.0, green: 1.0, blue: 166.0 / 255.0, alpha: 1.0)
+    }
+    
+    @nonobjc class var lightPink: UIColor {
+        return UIColor(red: 248.0 / 255.0, green: 246.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
+    
+}
