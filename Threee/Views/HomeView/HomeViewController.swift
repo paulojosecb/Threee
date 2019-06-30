26070c6431f02b773815c6fbfdd6c85f82f8976c
Paulo José <paulojocb@gmail.com>
2019-06-30 17:54:09 -0300

    Add new components to HomeView

--- Threee/Views/HomeView/HomeViewController.swift
+++ Threee/Views/HomeView/HomeViewController.swift
@@ -11,13 +11,42 @@ import UIKit
 class HomeViewController: UIViewController {
     
     var viewModel: HomeViewModel?
+    
+    lazy var dottedGrid: DottedGridView = {
+        return DottedGridView(frame: self.view.frame)
+    }()
+    
+    lazy var pageTitleLabelView: PageTitleLabelView = {
+        let view = PageTitleLabelView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-        viewModel = HomeViewModel(delegate: self)
         
+        viewModel = HomeViewModel(delegate: self)
         viewModel?.observerToday()
+        
+        setupViews()
+    }
+    
+    func setupViews() {
+        self.view.addSubview(dottedGrid)
+        self.view.addSubview(pageTitleLabelView)
+        
+        dottedGrid.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        dottedGrid.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        dottedGrid.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        dottedGrid.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        
+        pageTitleLabelView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: -3).isActive = true
+        pageTitleLabelView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 43.0).isActive = true
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        dottedGrid.drawDots()
     }
     
 }
